72fdf24b3c45de7d61e44b1adc75d5ce2205d0d9
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-05-09 17:45:42 +0600

    examples added

---
+++ src/example3.ts
@@ -0,0 +1,18 @@
+interface Person {
+
+     firstName: string;
+   
+     lastName: string;
+   
+   }
+   
+   
+   
+   function fullName<T extends Person>(person: T): string {
+   
+     return `${person.firstName} ${person.lastName}`;
+   
+   }
+
+const getName = fullName({firstName:"sekh", lastName:"saadi"})
+console.log(getName);
